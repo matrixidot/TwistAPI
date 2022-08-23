f79e55a8e14e39e1a5dc63c7724b4cee53642899
N3E0 <romirgade@gmail.com>
2022-08-22 23:15:36 -0400

    Updated JavaDocs

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"me.neo.twistapi","l":"CraftListener"},{"p":"me.neo.twistapi","l":"ItemBuilder"},{"p":"me.neo.twistapi","l":"RecipeBuilder"},{"p":"me.neo.twistapi","l":"TwistAPI"},{"p":"me.neo.twistapi","l":"TwistLinkingCommands"}];updateSearchResults();
